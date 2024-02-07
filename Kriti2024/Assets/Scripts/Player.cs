7c17b160e99a24d7ce8767c109fa8f2bad248bdc
karan-kumawat17 <118342257+karan-kumawat17@users.noreply.github.com>
2024-02-07 15:08:29 +0530

    made changes

--- Kriti2024/Assets/Scripts/Player.cs
+++ Kriti2024/Assets/Scripts/Player.cs
@@ -4,38 +4,42 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D ball;
     private Transform MuzzleTransform2D;
-    
+
     private Transform PlayerTransform2D, Portal1Transform2D, Portal2Transform2D;
-    
+
     void Start()
     {
         ball = GetComponent<Rigidbody2D>();
         MuzzleTransform2D = GameObject.FindGameObjectWithTag("Muzzle").GetComponent<Transform>();
         this.transform.position = MuzzleTransform2D.position;
-        
-        Portal2Transform2D = GameObject.FindGameObjectWithTag("PortalBlue2").GetComponent<Transform>(); 
-    // Start is called before the first frame update
+
+        Portal2Transform2D = GameObject.FindGameObjectWithTag("PortalBlue2").GetComponent<Transform>();
+        // Start is called before the first frame update
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    private void OnCollisionEnter2D(Collision2D other){
-        if(other.gameObject.layer == 8){
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.layer == 8)
+        {
             ball.gravityScale *= -1;
         }
-    
-    
+
+
     }
-    private void OnTriggerEnter2D(Collider2D other){
+}
+
+    /*private void OnTriggerEnter2D(Collider2D other){
         if(other.gameObject.layer == 10){
             this.transform.position = Portal2Transform2D.position;
         }
         
         }
-    }
+    }*/
     
 
